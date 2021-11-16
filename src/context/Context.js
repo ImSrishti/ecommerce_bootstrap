c48168f03f27b38e456f7cfbfbdeb1565d48deda
ImSrishti <shristi998@gmail.com>
2021-11-16 16:45:10 +0530

    Generated Json products

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,7 +1,18 @@
 import React, { createContext } from 'react'
-
+import faker from "faker";
 const Cart = createContext();
 const Context = ({children}) => {
+
+    const products = [...Array(20)].map(()=>({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        image: faker.random.image(),
+        inStock: faker.random.arrayElement([6,7,0,3,5]),
+        fastDelivery: faker.datatype.boolean(),
+        ratings: faker.random.arrayElement([1,2,3,4,5])
+    }))
+console.log(products)
     return (
         <Cart.Provider>
             {children}
