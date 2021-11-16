16a16fe12f8b163b52dc37898f529901ee29c38e
ImSrishti <shristi998@gmail.com>
2021-11-16 17:13:44 +0530

    Set Up Context API and useReducer Hook

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,6 +1,7 @@
-import React, { createContext } from 'react'
+import React,{useReducer} from 'react'
 import faker from "faker";
-const Cart = createContext();
+import { cartReducer } from './Reducer';
+const Cart = React.createContext();
 const Context = ({children}) => {
 
     const products = [...Array(20)].map(()=>({
@@ -12,12 +13,22 @@ const Context = ({children}) => {
         fastDelivery: faker.datatype.boolean(),
         ratings: faker.random.arrayElement([1,2,3,4,5])
     }))
+
+const [state, dispatch] = useReducer(cartReducer, {
+    products: products ,
+    cart:[]
+})
+
 console.log(products)
     return (
-        <Cart.Provider>
+        <Cart.Provider value={{state,dispatch}}>
             {children}
         </Cart.Provider>
     )
 }
-
+//in index.js this component has wrapped App component
 export default Context
+
+export const CartState = () =>{
+    return useContext(Cart); //useContext takes context so that we can access it accross the app
+}
