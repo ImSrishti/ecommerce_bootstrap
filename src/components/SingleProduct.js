c50deb71b69351b3e267da092de69fb5a9d6dfb6
ImSrishti <shristi998@gmail.com>
2021-11-17 12:24:25 +0530

    Add Remove Cart functionality from Home page done

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -2,8 +2,15 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
 import Rating from './Rating'
+import { CartState } from "../context/Context"
 import './style.css'
-const SingleProduct = ({ prod }) => {
+const SingleProduct = ({ prod }) => {   //single product is prop
+
+    const {
+        state: { cart },
+        dispatch
+    } = CartState();
+
     return (
         <div className="products">
             <Card>
@@ -21,10 +28,35 @@ const SingleProduct = ({ prod }) => {
                         <Rating rating={prod.ratings} />
 
                     </Card.Subtitle>
-                    <Button style={{padding:"5px"}} variant="danger">Remove from cart</Button>
-                    <Button style={{padding:"5px"}} disabled={!prod.inStock}>
-                        {!prod.inStock ? "Out Of Stock" : "Add to Cart"}
-                    </Button>
+                    {
+                        cart.some(p => p.id === prod.id) ? (
+                            <Button 
+                            onClick ={()=>{
+                                dispatch({
+                                    type : "REMOVE_FROM_CART",
+                                    payload: prod,
+                                })
+                            }}
+                            style={{ padding: "5px", marginRight: "5px" }} 
+                            variant="danger">
+                                Remove from cart
+                            </Button>
+                        ) : (
+                            <Button 
+                            onClick ={()=>{
+                                dispatch({
+                                    type : "ADD_TO_CART",
+                                    payload: prod,
+                                })
+                            }}
+                            style={{ padding: "5px" }} 
+                            disabled={!prod.inStock}>
+                                {!prod.inStock ? "Out Of Stock" : "Add to Cart"}
+                            </Button>
+                        )
+                    }
+
+
                 </Card.Body>
             </Card>
         </div>
@@ -32,3 +64,7 @@ const SingleProduct = ({ prod }) => {
 }
 
 export default SingleProduct
+
+
+//some tells us if that particular ele exist or not
+//filter returns elements some returns true
--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,8 +1,21 @@
 export const cartReducer = (state,action) => {
     switch (action.type) {
-       
+       case "ADD_TO_CART":
+           return {...state,
+            cart:[
+                ...state.cart,
+                {...action.payload, qty: 1}
+            ]}
+        case "REMOVE_FROM_CART":
+                return {...state,
+                 cart: state.cart.filter((c)=> c.id !== action.payload.id),
+                    
+                 }
     
         default:
             return state;
     }
 }
+
+//action contains two things one is type and second is payload ,
+//payload contains the data that we want to put in state and manipulate the state
